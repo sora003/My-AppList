014f5f308ce6f1bd64457b28eab6bb2aaa64e66c
sora003 <sumisoras@163.com>
2015-12-10 11:19:31 +0800

    V0.5.8-α
    修正了Revert错误导致的问题

--- MyAppList/app/src/main/java/com/sora/myapplist/MainActivity.java
+++ MyAppList/app/src/main/java/com/sora/myapplist/MainActivity.java
@@ -37,10 +37,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //初始化
         init();
-        AppService appService = new AppService(getApplicationContext());
-        //获取App信息
-        appService.createAppList();
-        appInfoList = appService.getAppInfoList();
+        createAppList();
         //将list加载到适配器
         AppInfoAdapter appInfoAdapter = new AppInfoAdapter(this, appInfoList);
         //显示程序列表
@@ -99,15 +96,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //导出程序列表
-    private void exportFile() throws IOException {
-        //判断SDCard是否存在并且可读写
-        Boolean isExisted = Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
-        //新建FileService对象
-        FileService service = new FileService(getApplicationContext());
-        //文件名
-        String filename = "My AppList.txt";
-        String filecontent = "";
+    private String getList(){
+        String app_list = "";
         for (int i=0;i<appInfoList.size();i++){
             //获取App的Label
             String appName = appInfoList.get(i).getAppName();
@@ -119,18 +109,92 @@ public class MainActivity extends AppCompatActivity {
             String installTime = appInfoList.get(i).getInstallTime();
             //获取版本名
             String editon = appInfoList.get(i).getEdition();
-            filecontent += (i + 1) + ":   " + appName + "    " + editon + "    " + appSize + "    " + packageName + "    " + installTime+"  \n";
+            app_list += (i + 1) + ":   " + appName + "    " + editon + "    " + appSize + "    " + packageName + "    " + installTime+"  \n";
         }
+        return app_list;
+    }
+    //导出程序列表
+    private void exportFile() throws IOException {
+        //判断SDCard是否存在并且可读写
+        Boolean isExisted = Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+        //新建FileService对象
+        FileService service = new FileService(getApplicationContext());
+        //文件名
+        String filename = "My AppList.txt";
+        String filecontent = getList();
         if (isExisted){
             service.saveToSDCard(filename,filecontent);
         }
-        Toast.makeText(getApplicationContext(), "已导出程序列表", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "已导出程序列表", Toast.LENGTH_SHORT).show();
     }
 
     //将程序列表复制到剪贴板
     private void copyFile() {
         ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        clipboardManager.setText(getList());
+    }
 
+    //获取AppList信息
+    public void createAppList() {
+        //获取已安装的应用程序包
+        List<PackageInfo> packs = getPackageManager().getInstalledPackages(0);
+        //对获得的应用程序包进行相关操作
+        for (int i = 0; i < packs.size(); i++) {
+            PackageInfo p = packs.get(i);
+            //过滤无版本名的app
+            if (p.versionName == null) {
+                continue;
+            }
+            //过滤非三方应用程序
+            if (!filterApp(p.applicationInfo)) {
+                continue;
+            }
+            //获得App创建时对应的文件夹
+            String dir = p.applicationInfo.publicSourceDir;
+            //获取App创建时对应文件夹的大小
+            Long size_long = new File(dir).length();
+            //定义时间格式
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            //App创建时对应的文件夹的最近修改时间-最近一次的更新时间
+            Date date = new Date(new File(dir).lastModified());
+            //获取App的Label
+            String appName = (String) p.applicationInfo.loadLabel(getPackageManager());
+            //获取App的大小
+            String appSize = Long.toString(size_long / 1024 / 1024) + "MB";
+            //获取App对应的包名
+            String packageName = p.packageName;
+            //获取App的安装时间
+            String installTime = formatter.format(date);
+            //获取版本名
+            String editon = p.versionName;
+            //构建AppInfo对象
+            AppInfo appInfo = new AppInfo();
+            //设置App所需输出的各参数值
+            appInfo.setAppName(appName);
+            appInfo.setEdition(editon);
+            appInfo.setPackageName(packageName);
+            appInfo.setAppSize(appSize);
+            appInfo.setInstallTime(installTime);
+            //添加进列表中
+            appInfoList.add(appInfo);
+            int now_progress = (int) ((i + 1) / (float) packs.size() * progressBar.getMax());
+            progressBar.setProgress(now_progress);
+        }
+        progressBar.setProgress(progressBar.getMax());
+        progressBar.setVisibility(View.GONE);
+    }
+
+    //三方应用程序过滤器
+    private boolean filterApp(ApplicationInfo applicationInfo) {
+        //代表被用户升级过的系统应用
+        if ((applicationInfo.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0) {
+            return true;
+        }
+        //代表用户应用
+        else if ((applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
+            return true;
+        }
+        return false;
     }
 
     //显示toolbar内容
